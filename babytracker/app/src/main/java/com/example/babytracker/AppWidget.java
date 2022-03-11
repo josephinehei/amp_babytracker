2816184127189a3e74047b7935529e461a1d6556
josephinehei <k.josephine.heinig@gmail.com>
2022-03-10 19:55:54 -0700

    working on widget

--- babytracker/app/src/main/java/com/example/babytracker/AppWidget.java
+++ babytracker/app/src/main/java/com/example/babytracker/AppWidget.java
@@ -12,13 +12,22 @@ public class AppWidget extends AppWidgetProvider {
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds){
         for(int appWidgetId : appWidgetIds) {
-            Intent intent = new Intent(context, MainActivity.class);
-            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0 );
+//            Intent intent = new Intent(context, MainActivity.class);
+//            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0 );
 
             RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget);
-            views.setOnClickPendingIntent(R.id.widgetButton1, pendingIntent);
+            views.setOnClickPendingIntent(R.id.widgetButton1, getPendingIntent(context, 0));
+            views.setOnClickPendingIntent(R.id.widgetButton2, getPendingIntent(context, 0));
+            views.setOnClickPendingIntent(R.id.widgetButton3, getPendingIntent(context, 0));
 
             appWidgetManager.updateAppWidget(appWidgetId, views);
         }
     }
+
+    private PendingIntent getPendingIntent(Context context, Integer value){
+        Intent intent = new Intent(context, MainActivity.class);
+        return PendingIntent.getActivity(context, value, intent, 0);
+    }
+
+
 }
